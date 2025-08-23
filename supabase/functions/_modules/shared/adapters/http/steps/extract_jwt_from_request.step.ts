a2f3d0a0834fca30a9a103281aaf17b6206e1b14
송태환 <axoghks@gmail.com>
2025-08-23 15:46:24 +0900

    feat: :sparkles: 요청 정보에서 JWT 토큰 추출하는 step 추가

---
+++ supabase/functions/_modules/shared/adapters/http/steps/extract_jwt_from_request.step.ts
@@ -0,0 +1,30 @@
+import { HttpException } from "../error/exception.ts";
+import { FirstStep } from "../../../core/chain.ts";
+import { RouteState } from "../../../types/state.types.ts";
+
+export type Token = {
+  value: string;
+  type: string;
+};
+
+export const extractJwtFromRequest = <
+  Body = unknown,
+  Query = unknown,
+  State extends RouteState<Body, Query> = RouteState<Body, Query>
+>(): FirstStep<Token, Body, Query, State> => {
+  return (ctx): Token => {
+    const headers = ctx.request.headers;
+    const property = headers.get("Authorization");
+
+    if (!property) {
+      throw HttpException.unauthorized("required authorization header");
+    }
+
+    const [type, value] = property.split(" ");
+
+    return {
+      value,
+      type,
+    };
+  };
+};
